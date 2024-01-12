d6a835dae2d5d6ee07ce5ab6efd75e909f168bc3
Hee Sang <118061713+codrin2@users.noreply.github.com>
2024-01-12 22:27:51 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 1124 ms, Memory: 87708 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+    public static int[] visit;
+    public static int cnt = 1;
+    public static Queue<Integer> queue = new LinkedList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int node = Integer.parseInt(st.nextToken());
+        int edge = Integer.parseInt(st.nextToken());
+        int start = Integer.parseInt(st.nextToken());
+        visit = new int[node + 1];
+
+        for (int i = 0; i < node+1; i++) {
+            graph.add(new ArrayList <Integer>());
+        }
+
+        for (int i = 0; i < edge; i++) {
+            st = new StringTokenizer(br.readLine());
+            int node1 = Integer.parseInt(st.nextToken());
+            int node2 = Integer.parseInt(st.nextToken());
+            graph.get(node1).add(node2);
+            graph.get(node2).add(node1);
+        }
+
+        for (int i = 1; i < node+1; i++) {
+            Collections.sort(graph.get(i),Collections.reverseOrder());
+        }
+
+        bfs(start);
+
+        for (int i = 1; i < node+1; i++) {
+            sb.append(visit[i]).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    public static void bfs(int node) {
+        queue.add(node);
+        visit[node] = cnt++;
+        while (queue.size() != 0) {
+            int now = queue.poll();
+            for (int i = 0; i < graph.get(now).size(); i++) {
+                int nowNode = graph.get(now).get(i);
+                if (visit[nowNode] == 0) {
+                    queue.add(nowNode);
+                    visit[nowNode] = cnt++;
+                }
+            }
+        }
+    }
+
+}

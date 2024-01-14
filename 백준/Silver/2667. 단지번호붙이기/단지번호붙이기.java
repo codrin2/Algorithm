d6c37de57a64b84540a4cc413c77991f4fbbd318
Hee Sang <118061713+codrin2@users.noreply.github.com>
2024-01-15 00:26:26 +0900

    [Silver I] Title: 단지번호붙이기, Time: 124 ms, Memory: 14484 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,63 +1,87 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class Main {
-	
-	static int[][] danji;
-	static boolean[][] visited;
-	static int[] dx = {0,0,-1,1};
-	static int[] dy = {-1,1,0,0};
-	static List<Integer> result;
-	static int cnt, N;
+    public static int[][] arr, visit;
+    public static int cnt, n;
 
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		result = new LinkedList<>(); 
-		N = Integer.parseInt(br.readLine());
-		danji = new int[N][N];
-		visited = new boolean[N][N];
-		cnt = 1; 
-		
-		for(int i=0;i<N;i++) {
-			String str = br.readLine();
-			for(int j=0;j<N;j++) {
-				danji[i][j] = str.charAt(j)-'0';
-			}
-		}
-		
-		
-		for(int x=0;x<N;x++) {
-			for(int y=0;y<N;y++) {
-				if(danji[x][y]==1 && !visited[x][y]) {
-					dfs(x,y);
-					result.add(cnt);
-					cnt = 1;
-				}
-			}
-		}
-		
-		Collections.sort(result);
-		
-		bw.write(result.size()+"\n");
-		for(int r : result) bw.write(r+"\n");
-		bw.flush();
-		bw.close();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n][n];
+        visit = new int[n][n];
+        int[] result = new int[n*n];
+        int resultCnt = 0;
 
-	}
-	
-	public static void dfs(int x, int y) {
-		visited[x][y] = true;
-		
-		for(int i=0;i<4;i++) {
-			int nx = dx[i]+x;
-			int ny = dy[i]+y;
-			
-			if(nx>=0 && ny>=0 && nx<N && ny<N && !visited[nx][ny] && danji[nx][ny]==1) {
-				cnt++;
-				dfs(nx,ny);
-			}
-		}
-	}
+        for (int i = 0; i < n; i++) {
+            String temp = br.readLine();
+            int j =0;
+            for (String s : temp.split("")) {
+                arr[i][j++] = Integer.parseInt(s);
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (arr[i][j] == 1) {
+                    if (visit[i][j] == 0) {
+                        cnt=1;
+                        visit[i][j] = 1;
+                        find(i, j);
+                        result[resultCnt++] = cnt;
+                    }
+                }
+            }
+        }
+        int[] real = new int[resultCnt];
+        for (int i = 0; i < resultCnt; i++) {
+            real[i] = result[i];
+        }
+        Arrays.sort(real);
+        sb.append(resultCnt).append("\n");
+        for (int i = 0; i < resultCnt; i++) {
+            sb.append(real[i]).append("\n");
+        }
+        
+        System.out.println(sb);
+
+    }
+
+    public static void find(int x, int y) {
+        if (x < n-1) {
+            if (arr[x+1][y] == 1 && visit[x + 1][y] == 0) {
+                cnt++;
+                visit[x+1][y] =1;
+                find(x+1, y);
+            }
+        }
+
+        if (y < n - 1) {
+            if (arr[x][y+1] == 1 && visit[x][y + 1] == 0) {
+                cnt++;
+                visit[x][y+1] =1;
+                find(x, y+1);
+            }
+        }
+
+        if (x > 0) {
+            if (arr[x-1][y] == 1 && visit[x - 1][y] == 0) {
+                cnt++;
+                visit[x-1][y] =1;
+                find(x-1, y);
+            }
+        }
+
+        if (y > 0) {
+            if (arr[x][y-1] == 1 && visit[x][y - 1] == 0) {
+                cnt++;
+                visit[x][y-1] =1;
+                find(x, y-1);
+            }
+        }
+    }
 }

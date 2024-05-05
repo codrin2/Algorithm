34cee1a82e3695a14d1c9c7be56fc6b78f1f51d0
Hee Sang <118061713+codrin2@users.noreply.github.com>
2024-05-06 01:07:10 +0900

    [Gold IV] Title: 공유기 설치, Time: 316 ms, Memory: 29056 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -1,55 +1,41 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
- 
-import java.util.Arrays;
- 
+import java.io.*;
+import java.util.*;
 public class Main {
-	
-	public static int[] house;
-	
-	public static void main(String[] args) throws IOException {		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));		
-		StringTokenizer st = new StringTokenizer(br.readLine()," ");
-		
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		
-		house = new int[N];
-		
-		for(int i = 0; i < N; i++) {
-			house[i] = Integer.parseInt(br.readLine());
-		}
-		
-		Arrays.sort(house);	
-		int lo = 1;	
-		int hi = house[N - 1] - house[0] + 1;	
-		
-		while(lo < hi) {	
-			
-			int mid = (hi + lo) / 2;
-			if(canInstall(mid) < M) {
-				hi = mid;
-			}
-			else {
-				lo = mid + 1;
-			}
-		}
-		System.out.println(lo - 1);
-	}
-	public static int canInstall(int distance) {
-		
-		int count = 1;
-		int lastLocate = house[0];
-		
-		for(int i = 1; i < house.length; i++) {
-			int locate = house[i];
-			if(locate - lastLocate >= distance) {
-				count++;
-				lastLocate = locate;
-			}
-		}
-		return count;		
-	}	
+    public static int N, C;
+    public static int[] home;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        home = new int[N];
+        for(int i=0;i<N;i++){
+            home[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(home);
+
+        int min = 1;
+        int max = home[N-1] - home[0]+1;
+        while(min < max){
+            int mid = (min+max)/2;
+            if(canInstall(mid) < C){
+                max = mid;
+            }else{
+                min = mid + 1;
+            }
+        }
+        System.out.println(min - 1);
+
+    }
+    public static int canInstall(int mid){
+        int before = home[0];
+        int cnt = 1;
+        for(int i =1;i<N;i++){
+            if(home[i]- before >= mid){
+                before = home[i];
+                cnt++;
+            }
+        }
+        return cnt;
+    }
 }

ee33186f3f6d2a7c563dd4d7987e714c747b8327
Hee Sang <118061713+codrin2@users.noreply.github.com>
2025-05-13 22:28:03 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 828 ms, Memory: 161368 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -1,73 +1,87 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.StringTokenizer;
+
 
 public class Main {
-    public static Queue<int[]> queue = new LinkedList<>();
-    ;
-    public static int[][] arr;
-    public static int[][][] visit;
-    public static int column, row;
-    public static int[] dx = {0, 1, 0, -1};
-    public static int[] dy = {1, 0, -1, 0};
+    static class Loc{
+        int i;
+        int j;
+        int cnt;
+        boolean destroyed;
+
+        public Loc(int i, int j, int cnt, boolean d) {
+            this.i = i;
+            this.j = j;
+            this.cnt = cnt;
+            this.destroyed = d;
+        }
+    }
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        column = Integer.parseInt(st.nextToken());
-        row = Integer.parseInt(st.nextToken());
-        arr = new int[column][row];
-        visit = new int[column][row][2];
+        String[] inputs = br.readLine().split(" ");
 
-        for (int i = 0; i < column; i++) {
-            String temp = br.readLine();
-            for (int j=0;j<row;j++) {
-                arr[i][j] = Integer.parseInt(String.valueOf(temp.charAt(j)));
+        int n = Integer.parseInt(inputs[0]);
+        int m = Integer.parseInt(inputs[1]);
+
+        char[][] map = new char[n][m];
+        for (int i = 0; i < n; i++) {
+            String input = br.readLine();
+            for (int j = 0; j < m; j++) {
+                map[i][j] = input.charAt(j);
             }
         }
-        queue.add(new int[]{0, 0, 0});
-        visit[0][0][0]=1;
-        bfs();
 
-        if (visit[column - 1][row - 1][0] == 0 && visit[column - 1][row - 1][1] == 0) {
-            System.out.println(-1);
-        } else if (visit[column - 1][row - 1][0] == 0) {
-            System.out.println(visit[column - 1][row - 1][1] );
-        } else if (visit[column - 1][row - 1][1] == 0) {
-            System.out.println(visit[column - 1][row - 1][0] );
-        } else {
-            System.out.println(Math.min(visit[column - 1][row - 1][0] , visit[column - 1][row - 1][1]));
-        }
 
-    }
+        Queue<Loc> q = new LinkedList<>();
+        q.add(new Loc(0, 0, 1, false));
 
-    public static void bfs() {
-        while (queue.size() != 0) {
-            int[] temp = queue.poll();
-            int nowX = temp[0];
-            int nowY = temp[1];
-            int flag = temp[2];
-            for (int i = 0; i < 4; i++) {
-                int nextX = nowX + dx[i];
-                int nextY = nowY + dy[i];
-                if (nextX >= 0 && nextY >= 0 && nextX < column && nextY < row) {
-                    if (visit[nextX][nextY][flag] == 0) {
-                        if (arr[nextX][nextY] == 1 && flag == 0) {
-                            visit[nextX][nextY][1] = visit[nowX][nowY][flag] + 1;
-                            if (nextX == column - 1 && nextY == row - 1) return;
-                            queue.add(new int[]{nextX, nextY, 1});
-                        } else if (arr[nextX][nextY] == 0) {
-                            visit[nextX][nextY][flag] = visit[nowX][nowY][flag] + 1;
-                            if (nextX == column - 1 && nextY == row - 1) return;
-                            queue.add(new int[]{nextX, nextY, flag});
-                        }
+        int[] mi = {0, 0, -1, 1};
+        int[] mj = {-1, 1, 0, 0};
+
+        boolean[][][] visited = new boolean[n][m][2];
+
+        while (!q.isEmpty()) {
+            Loc now = q.poll();
+
+            if (now.i == n - 1 && now.j == m - 1) {
+                System.out.println(now.cnt);
+                return;
+            }
+
+            for (int d = 0; d < 4; d++) {
+                int ni = now.i + mi[d];
+                int nj = now.j + mj[d];
+
+                if(ni<0 || ni>=n || nj<0 || nj>=m) continue;
+
+                int next_cnt = now.cnt+1;
+
+                if(map[ni][nj]=='0'){ // 벽이 아니면
+                    if(!now.destroyed && !visited[ni][nj][0]) { // 부신 벽이 여태까지 없었으면
+                        q.add(new Loc(ni, nj, next_cnt, false));
+                        visited[ni][nj][0] = true;
+                    }else if(now.destroyed && !visited[ni][nj][1]){ // 벽을 한번 부신 적이 있으면
+                        q.add(new Loc(ni, nj, next_cnt, true));
+                        visited[ni][nj][1] = true;
                     }
+
+                }else if(map[ni][nj]=='1'){ // 벽이면
+
+                    if(!now.destroyed){ // 한번도 벽을 부순적이 없다면 부순다~
+                        q.add(new Loc(ni, nj, next_cnt, true));
+                        visited[ni][nj][1] = true;
+                    }
+                    // 한번 부순 적이 있다면 또 부수고 갈 수 없기 때문에 pass
                 }
             }
+
         }
+
+        System.out.println(-1);
     }
 }
